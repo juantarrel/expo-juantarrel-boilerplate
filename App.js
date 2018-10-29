cdacf6185927823a2ea68cbbd95a1bfe71476721
Juan Tarrel <juantarrel@gmail.com>
2018-10-29 13:22:35 -0700

    Apploading for caching images and loading staff on splash screen

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Asset, AppLoading } from 'expo';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isReady: false,
+    }
+  }
+
   render() {
+
+      if (!this.state.isReady) {
+          return (
+              <AppLoading
+                  startAsync={ this.cacheResourcesAsync }
+                  onFinish={() => this.setState({ isReady: true })}
+                  onError={console.warn}
+              />
+          );
+      }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Boilerplate tarrel</Text>
       </View>
     );
   }
+
+    async cacheResourcesAsync() {
+        const images = [
+            // All images cached here
+            // require('./assets/...'),
+        ];
+
+        const cacheImages = images.map((image) => {
+            return Asset.fromModule(image).downloadAsync();
+        });
+        return Promise.all(cacheImages);
+    }
+
 }
 
 const styles = StyleSheet.create({
